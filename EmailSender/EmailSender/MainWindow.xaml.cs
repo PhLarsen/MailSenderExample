8077d0799516da602ec811305a7d3a4d675f0c28
Philip Larsen <pcln3@hotmail.com>
2017-11-22 21:40:05 +0100

    Mail example

---
+++ EmailSender/EmailSender/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Mail;
+using System.Net;
+
+namespace EmailSender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSendMail_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress(txtBoxFrom.Text);
+                mailMessage.To.Add(txtBoxRecipient.Text);
+                mailMessage.Subject = txtBoxSubject.Text;
+                mailMessage.Body = txtBoxMessage.Text;
+
+
+                SmtpClient smtp = new SmtpClient("smtp.live.com");
+                smtp.EnableSsl = true;
+                smtp.Credentials = new NetworkCredential(txtBoxFrom.Text, passwordBox.Password);
+                smtp.Port = 587;
+                smtp.Send(mailMessage);
+
+                MessageBox.Show("Mail sendt");
+                
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            
+
+        }
+    }
+}
